b86e9d1274c66d32dc14f8cc91fdba4efce698bb
DAZELPRO <dazelpro@gmail.com>
2023-06-06 16:19:37 +0700

    generate root routing

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+	{
+		path: 'auth',
+		pathMatch: 'full',
+		loadChildren: () =>
+			import('./modules/auth/auth.module').then(
+				(module) => module.AuthModule
+			),
+	},
+	{
+		path: '',
+		// canActivate: [AuthGuard],
+		loadChildren: () =>
+			import('./modules/layout/layout.module').then(
+				(module) => module.LayoutModule
+			),
+	},
+];
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    imports: [
+        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    ],
+    exports: [CommonModule, RouterModule],
 })
 export class RoutingModule {}
