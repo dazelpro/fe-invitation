9d031b7f885367771c2e95143af577ed0a10d219
DAZELPRO <dazelpro@gmail.com>
2023-06-15 22:41:08 +0700

    styling

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './page/login/login.component';
 import { AuthRoutingModule } from './auth.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ChangePasswordComponent } from './page/change-password/change-password.component';
 
 @NgModule({
-    declarations: [LoginComponent],
+    declarations: [LoginComponent, ChangePasswordComponent],
     imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
