0e3e1912e06c0442522e41ac3ad113f537aa0ab8
DAZELPRO <dazelpro@gmail.com>
2023-06-22 14:40:49 +0700

    + error session expired

--- src/app/shared/dialogs/token-expired/token-expired.component.ts
+++ src/app/shared/dialogs/token-expired/token-expired.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-token-expired',
-  templateUrl: './token-expired.component.html',
-  styleUrls: ['./token-expired.component.css']
+    selector: 'app-token-expired',
+    templateUrl: './token-expired.component.html',
+    styleUrls: ['./token-expired.component.css']
 })
 export class TokenExpiredComponent implements OnInit {
+    constructor(public dialogRef: MatDialogRef<TokenExpiredComponent>) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    close() {
+        this.dialogRef.close();
+    }
 }
