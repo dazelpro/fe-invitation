36728e749a9b45dec10e6b8842e80404eee97e31
DAZELPRO <dazelpro@gmail.com>
2023-06-23 16:47:13 +0700

    +header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputComponent } from './input/input.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TokenExpiredComponent } from './dialogs/token-expired/token-expired.component';
+import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
-    declarations: [InputComponent, TokenExpiredComponent],
-    imports: [CommonModule, FormsModule],
-    exports: [InputComponent]
+    declarations: [
+        InputComponent,
+        TokenExpiredComponent,
+        HeaderComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    exports: [
+        InputComponent,
+        HeaderComponent
+    ]
 })
 export class SharedModule {}
