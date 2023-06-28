295e0034cc64a746ef5185ddc383cd39af308d85
DAZELPRO <dazelpro@gmail.com>
2023-06-28 15:49:22 +0700

    get orderlist api

--- src/app/modules/client/page/order-list/order-list.component.ts
+++ src/app/modules/client/page/order-list/order-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MyOrder } from '../../../../core/models/order.model';
+import { OrderService } from '../../../../core/services/order.service';
 
 @Component({
-  selector: 'app-order-list',
-  templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+    selector: 'app-order-list',
+    templateUrl: './order-list.component.html',
+    styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
+    orderList: MyOrder;
 
-  constructor() { }
+    constructor(private orderService: OrderService, private _snackBar: MatSnackBar) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.fetchMyOrder();
+    }
 
+    fetchMyOrder() {
+        this.orderService.getMyOrder().subscribe({
+            next: (r) => {
+                console.log(r);
+                this.orderList = r.data.order;
+            },
+            error: (e) => {
+                console.log(e);
+                this._snackBar.open('Terdapat kendala pada sistem');
+            }
+        });
+    }
 }
