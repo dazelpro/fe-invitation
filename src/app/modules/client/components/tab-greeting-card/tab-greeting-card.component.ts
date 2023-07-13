4e2691a36ebaa08765aeb04390e18f9f805af68d
DAZELPRO <dazelpro@gmail.com>
2023-07-13 18:10:12 +0700

    greeting card

--- src/app/modules/client/components/tab-greeting-card/tab-greeting-card.component.ts
+++ src/app/modules/client/components/tab-greeting-card/tab-greeting-card.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Greeting } from '../../../../core/models/client.model';
+import { ClientService } from '../../../../core/services/client.service';
+import { CommonService } from '../../../../core/services/common.service';
+import { StorageService } from '../../../../core/services/storage.service';
 
 @Component({
     selector: 'app-tab-greeting-card',
@@ -6,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./tab-greeting-card.component.css']
 })
 export class TabGreetingCardComponent implements OnInit {
-    constructor() {}
+    id;
+    listGreeting: Greeting;
+    constructor(
+        public dialog: MatDialog,
+        private clientService: ClientService,
+        private _snackBar: MatSnackBar,
+        private storageService: StorageService,
+        private router: Router,
+        public commonService: CommonService
+    ) {}
 
     ngOnInit(): void {
-        console.log('ini greeting');
+        this.id = this.storageService.getIdParam('id-order-param');
+        if (!this.id) {
+            this.router.navigate(['/client']);
+        } else {
+            this.fetchGreeting();
+        }
+    }
+
+    fetchGreeting() {
+        let params = {
+            idOrder: this.id
+        };
+        this.clientService.getGreeting(params).subscribe({
+            next: (r) => {
+                this.listGreeting = r.data.greeting;
+            },
+            error: (e) => {
+                console.log(e);
+                this._snackBar.open('Terdapat kendala pada sistem');
+            }
+        });
     }
 }
